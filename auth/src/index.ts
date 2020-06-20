338196ccfbfcbdbb5fc0c1dd603904dd9a032404
unknown <breldanmuturi001@gmail.com>
2020-06-20 22:11:44 +0300

    Solving the requests error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -14,6 +14,6 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+app.listen(3001, () => {
+  console.log("Listening on port 3001!!!!");
 });
